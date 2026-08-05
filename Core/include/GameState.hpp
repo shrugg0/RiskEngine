ed212393bc40b809f36b900dde924b35f3b12820
shrugg0 <fabio.lucky04@gmail.com>
2026-08-05 12:08:29 +0200

    Done and tested  all the GameState class, everthing works

--- Core/include/GameState.hpp
+++ Core/include/GameState.hpp
@@ -17,13 +17,16 @@ private:
     std::map<std::string, TerritoryState> info;
     std::string fileName;
     std::ifstream dataGame;
+    std::string trim(std::string s);
 public:
     GameState(const std::string &filename);
 
     std::string getOwner(const std::string &territoy);
 
     int getTanks(const std::string &territory);
-    //?
-    std::vector<std::string> getListTerrOfPlayer(const std::string &territory);
+  
+    std::vector<std::string> getListTerrOfPlayer(const std::string &player);
+
+    void loadData();
     
 };
--- Core/src/GameState.cpp
+++ Core/src/GameState.cpp
@@ -19,17 +19,53 @@ GameState::GameState(const std::string &filename)
     
 }
 
+std::string GameState::trim(std::string s)
+{
+    while (!s.empty() && s[0] == ' ')
+        s.erase(0, 1);
+    while (!s.empty() && s.back() == ' ')
+        s.pop_back();
+    return s;
+}
+
+
 std::string GameState::getOwner(const std::string &territory)
 {
-    return std::string();
+    return info[territory].owner;
 }
 
 int GameState::getTanks(const std::string &territory)
 {
-    return 0;
+    return info[territory].tanks;
 }
 
-std::vector<std::string> GameState::getListTerrOfPlayer(const std::string &territory)
+void GameState::loadData()
 {
-    return std::vector<std::string>();
+    std::string line;
+
+    while (getline(dataGame, line))
+    {
+        int pos = line.find(':');
+
+        std::string state = trim(line.substr(0, pos));
+        std::string other = trim(line.substr(pos + 1));
+
+        int newPos = other.find(',');
+        info[state].owner = other.substr(0, newPos);
+        info[state].tanks = std::stoi(other.substr(newPos + 1));
+    }
+    
+}
+
+std::vector<std::string> GameState::getListTerrOfPlayer(const std::string &player)
+{
+    std::vector<std::string> risultato;
+    for (auto& pair : info)  // stato = la tua map<string, TerritoryState>
+    {
+        if (pair.second.owner == player)
+        {
+            risultato.push_back(pair.first);
+        }
+    }
+    return risultato;
 }
